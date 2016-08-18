4c1a33ff2c454259744d059c7eaea1a25c940a19
赤井橋 健 <titou5273@gmail.com>
2016-08-18 22:42:00 +0900

    新規作成

---
+++ ios_picture_upload/ios_picture_upload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_picture_upload
+//
+//  Created by 赤井橋 健2 on 2016/08/18.
+//  Copyright © 2016年 ken akaibashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
